0b7021673eeed179f515f3254c6d7e4a2c12de7d
k4sh4n1 <kashani@alum.sharif.edu>
2025-10-03 04:34:12 -0400

    Fix error on Windows command prompt: `$(pwd)`

--- container/container_run.bat
+++ container/container_run.bat
@@ -1,1 +1,1 @@
-podman run -d --name algorithm-server -p 8080:8080 -v $(pwd)/algorithm:/app/algorithm:ro algorithm-server:latest
+podman run -d --name algorithm-server -p 8080:8080 -v ./algorithm:/app/algorithm:ro algorithm-server:latest
