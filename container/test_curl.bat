0b0a3390d127e9e1d26eb2d15483457195972bb8
k4sh4n1 <kashani@alum.sharif.edu>
2025-10-03 04:49:16 -0400

    Debug `curl`

--- container/test_curl.bat
+++ container/test_curl.bat
@@ -1,9 +1,9 @@
-curl http://localhost:8000/health
+curl http://localhost:8080/
 
-curl http://localhost:8000/algorithms
+curl http://localhost:8080/health
 
-curl -X POST http://localhost:8000/001 -H "Content-Type: application/json" -d '[100, 102, 101, 103, 105, 104, 106, 108, 107, 109, 111, 110, 112, 114, 113]'
+curl -X POST http://localhost:8080/algorithm/001 -H "Content-Type: application/json" -d "{\"data\": [1, 2, 3, 4, 5]}"
 
-curl -X POST http://localhost:8000/002 -H "Content-Type: application/json" -d '[100, 102, 101, 103, 105, 104, 106, 108, 107, 109, 111, 110, 112, 114, 113]'
+curl -X POST http://localhost:8080/algorithm/002 -H "Content-Type: application/json" -d "{\"signal\": [1, 2, 3, 4, 5]}"
 
-curl -X POST http://localhost:8000/003 -H "Content-Type: application/json" -d '[100, 102, 101, 103, 105, 104, 106, 108, 107, 109, 111, 110, 112, 114, 113]'
+curl -X POST http://localhost:8080/algorithm/003 -H "Content-Type: application/json" -d "{\"signal\": [1, 2, 3, 4, 5]}"
