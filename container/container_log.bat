3a97be23969bba3c5c15548527dfbe2681ae0c14
k4sh4n1 <kashani@alum.sharif.edu>
2025-10-03 04:48:55 -0400

    Container log & restart

---
+++ container/container_log.bat
@@ -0,0 +1,5 @@
+podman ps
+podman stats algorithm-server
+podman inspect algorithm-server
+podman logs -f algorithm-server
+podman exec algorithm-server ls -la /app/algorithm
---
+++ container/container_restart.bat
@@ -0,0 +1,1 @@
+podman restart algorithm-server
