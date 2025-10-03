ee33e8432fefa9061d1dd53f96f534b19c8112e2
k4sh4n1 <kashani@alum.sharif.edu>
2025-10-03 04:54:27 -0400

    Fix `curl`

--- container/test_curl.bat
+++ container/test_curl.bat
@@ -2,8 +2,8 @@ curl http://localhost:8080/
 
 curl http://localhost:8080/health
 
-curl -X POST http://localhost:8080/algorithm/001 -H "Content-Type: application/json" -d "{\"data\": [1, 2, 3, 4, 5]}"
+curl -X POST http://localhost:8080/algorithm/001 -H "Content-Type: application/json" -d "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
 
-curl -X POST http://localhost:8080/algorithm/002 -H "Content-Type: application/json" -d "{\"signal\": [1, 2, 3, 4, 5]}"
+curl -X POST http://localhost:8080/algorithm/002 -H "Content-Type: application/json" -d "[1, 2, 3, 4, 5]"
 
-curl -X POST http://localhost:8080/algorithm/003 -H "Content-Type: application/json" -d "{\"signal\": [1, 2, 3, 4, 5]}"
+curl -X POST http://localhost:8080/algorithm/003 -H "Content-Type: application/json" -d "[1, 2, 3, 4, 5]"
