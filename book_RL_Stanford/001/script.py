741abac37c42a1433182020ec7595ccf21eb4c79
k4sh4n1 <kashani@alum.sharif.edu>
2025-11-21 02:56:56 -0500

    Rename class: capitalize

--- book_RL_Stanford/001/script.py
+++ book_RL_Stanford/001/script.py
@@ -5,22 +5,22 @@ import numpy as np
 
 
 @dataclass
-class process:
+class Process:
     @dataclass
     class State:
         price: float
 
     @staticmethod
     def next_state(st: State) -> State:
-        new_state = process.State(price=st.price + 1.0)
+        new_state = Process.State(price=st.price + 1.0)
         return new_state
 
 
-def simulation(ps: process, start_st: process.State):
+def simulation(ps: Process, start_st: Process.State):
     state = start_st
     while True:
         yield state
-        state = process.next_state(state)
+        state = Process.next_state(state)
 
 
 print(
@@ -28,7 +28,7 @@ print(
         (
             s.price
             for s in itertools.islice(
-                simulation(ps=process(), start_st=process.State(price=0.0)),
+                simulation(ps=Process(), start_st=Process.State(price=0.0)),
                 100,
             )
         ),
