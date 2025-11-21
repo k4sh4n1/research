fe70ec74d37f4f28b007575c40c5b738a22f8927
k4sh4n1 <kashani@alum.sharif.edu>
2025-11-21 02:42:07 -0500

    Static method will NOT need any instantiation

--- book_RL_Stanford/001/script.py
+++ book_RL_Stanford/001/script.py
@@ -16,22 +16,21 @@ class process:
         return new_state
 
 
-def simulation(process, start_state):
-    state = start_state
+def simulation(ps: process, start_st: process.state):
+    state = start_st
     while True:
         yield state
         state = process.next_state(state)
 
 
-ps = process()
-start_st = process.state(price=0.0)
 print(
     np.array(
         np.fromiter(
             (
                 s.price
                 for s in itertools.islice(
-                    simulation(process=ps, start_state=start_st), 100
+                    simulation(ps=process(), start_st=process.state(price=0.0)),
+                    100,
                 )
             ),
             float,
