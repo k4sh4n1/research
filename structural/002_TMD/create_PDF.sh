3ddaa3e0bc177f09f0e498e1bb9d9090ecf9fea8
k4sh4n1 <kashani@alum.sharif.edu>
2025-12-12 20:42:03 +0330

    Comment

--- structural/002_TMD/create_PDF.sh
+++ structural/002_TMD/create_PDF.sh
@@ -4,4 +4,4 @@ pandoc README.md -o README.pdf
 
 # To print my code,
 # I just dragged my Python files into the Brave browser and printed them from within Brave.
-# Then, merged them by https://bentopdf.com/
+# Then, merged PDF files by https://bentopdf.com/
