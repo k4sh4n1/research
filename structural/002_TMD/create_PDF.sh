025799c58080b107743c884646c250db4b67bdc2
k4sh4n1 <149871443+k4sh4n1@users.noreply.github.com>
2025-12-12 14:58:33 +0330

    https://bentopdf.com/

--- structural/002_TMD/create_PDF.sh
+++ structural/002_TMD/create_PDF.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 pandoc README.md -o README.pdf
+
+# I just dragged my Python files into the Brave browser and printed them from within Brave.
+# Then, merged them by https://bentopdf.com/#tools-header
+
