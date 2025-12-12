51c994e30ad88a0c5b7eb260ba733bf104d75cdd
k4sh4n1 <kashani@alum.sharif.edu>
2025-12-12 11:47:36 +0330

    Python code to PDF: did not work

---
+++ structural/002_TMD/create_PDF-code.sh
@@ -0,0 +1,9 @@
+# Convert Python file to PDF (requires LaTeX)
+pandoc script1_modal.py -o script1_modal.py.pdf --highlight-style=tango
+pandoc script1_OpenSees.py -o script1_OpenSees.py.pdf --highlight-style=tango
+pandoc script2_num.py -o script2_num.py.pdf --highlight-style=tango
+pandoc script2_num_.py -o script2_num_.py.pdf --highlight-style=tango
+pandoc script2_OpenSees.py -o script2_OpenSees.py.pdf --highlight-style=tango
+pandoc script2_OpenSees_.py -o script2_OpenSees_.py.pdf --highlight-style=tango
+pandoc script3_OpenSees.py -o script3_OpenSees.py.pdf --highlight-style=tango
+pandoc script4_OpenSees.py -o script4_OpenSees.py.pdf --highlight-style=tango
